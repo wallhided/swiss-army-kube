4cef427371c151441fbbb8e733fd13876e0851f7
Dmitriy Miroshnik <dmiroshnik@provectus.com>
2021-11-15 15:32:24 +0300

    fix efk p1

--- examples/argocd/main.tf
+++ examples/argocd/main.tf
@@ -58,3 +58,136 @@ module "argocd" {
     "server.ingress.paths[0]" = "/"
   }
 }
+
+module "efk" {
+  depends_on   = [module.argocd]
+  source       = "github.com/provectus/sak-efk"
+  cluster_name = module.kubernetes.cluster_name
+  argocd       = module.argocd.state
+  domains      = local.domain
+  kibana_conf = {
+    "ingress.annotations.kubernetes\\.io/ingress\\.class"               = "nginx"
+    "ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/auth-url"    = "https://auth.example.com/oauth2/auth"
+    "ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/auth-signin" = "https://auth.example.com/oauth2/sign_in?rd=https://$host$request_uri"
+  }
+  filebeat_conf = {
+    "setup.kibana" = {
+      "host" = "https://kibana.example.com:443"
+    }
+    "setup.dashboards.enabled" = true
+    "setup.template.enabled"   = true
+    "setup.template.name"      = "filebeat"
+    "setup.template.pattern"   = "filebeat-*"
+    "setup.template.settings" = {
+      "index.number_of_shards" = 1
+    }
+    "setup.ilm.enabled"      = "auto"
+    "setup.ilm.check_exists" = false
+    "setup.ilm.overwrite"    = true
+
+    "filebeat.modules" = [{
+      "module" = "system"
+      "syslog" = {
+        "enabled" : true
+      }
+      #var.paths: ["/var/log/syslog"]
+      "auth" = {
+        "enabled" : true
+        #var.paths: ["/var/log/authlog"]
+      }
+    }]
+    "filebeat.inputs" = [{
+      "type" = "container"
+      "paths" = [
+        "/var/log/containers/*.log"
+      ]
+      "stream" = "all"
+      "processors" = [
+        {
+          "add_kubernetes_metadata" = {
+            "host" = "$${NODE_NAME}"
+            "matchers" = [
+              { "logs_path" = {
+                "logs_path" : "/var/log/containers/"
+                }
+
+            }]
+          }
+        }
+      ]
+      }
+    ]
+    "filebeat.autodiscover" = [{
+      "providers" = [{
+        "type"          = "kubernetes"
+        "hints.enabled" = true
+        "templates" = [{
+          #Get logs only from pod with label logging=true
+          "condition.equals" = {
+            "kubernetes.labels.logging" : true
+          }
+          "config" = [{
+            "type" = "container"
+            "paths" = [
+              "/var/log/containers/*-test.log"
+            ]
+            "exclude_lines" = ["^\\s+[\\-`('.|_]"] # drop asciiart lines
+          }]
+          #Parse logs with Nginx grok pattern from container with label logtype=nginx
+          "condition.equals" = [{
+            "kubernetes.labels.logtype" = "nginx"
+            "config" = [{
+              "module" = "nginx"
+              "access" = {
+                "enabled" : "true"
+                "var.paths" : ["/var/log/nginx/access.log*"]
+              }
+              "error" = {
+                "enabled" : true
+                "var.paths" : ["/var/log/nginx/error.log*"]
+              }
+            }]
+          }]
+        }]
+      }]
+    }]
+    "processors" = [{
+      "add_cloud_metadata" = "~"
+      "decode_json_fields" = {
+        "fields"         = ["message"]
+        "process_array"  = false
+        "max_depth"      = 1
+        "target"         = ""
+        "overwrite_keys" = true
+        "add_error_key"  = true
+      }
+      #Drop all logs without labels logging=true
+      "drop_event" = {
+        "when" = {
+          "not" = {
+            "contains" = {
+              "kubernetes.pod.labels.logging" = "true"
+            }
+          }
+        }
+      }
+      #Drop health checks logs
+      "drop_event" = {
+        "when" = {
+          "regexp" = {
+            "message" = "'(?i)kube-probe/1.18+'"
+          }
+        }
+      }
+      "drop_event" = {
+        "when" = {
+          "regexp" = {
+            "message" = "'(?i)ELB-HealthChecker/2.0'"
+          }
+        }
+      }
+      }
+    ]
+
+  }
+}
