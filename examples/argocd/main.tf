9b053320b8de3b05c9bd302cd62183b3581e83be
Dmitriy Miroshnik <dmiroshnik@provectus.com>
2021-11-18 12:22:35 +0300

    fix efk p15

--- examples/argocd/main.tf
+++ examples/argocd/main.tf
@@ -58,3 +58,161 @@ module "argocd" {
     "server.ingress.paths[0]" = "/"
   }
 }
+
+module "efk" {
+  depends_on      = [module.argocd]
+  source          = "../../../../sak/sak-efk"
+  cluster_name    = module.kubernetes.cluster_name
+  argocd          = module.argocd.state
+  domains         = local.domain
+  elasticReplicas = 2
+  kibana_conf = {
+    "ingress.annotations.kubernetes\\.io/ingress\\.class"               = "nginx"
+    "ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/auth-url"    = "https://auth.example.com/oauth2/auth"
+    "ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/auth-signin" = "https://auth.example.com/oauth2/sign_in?rd=https://$host$request_uri"
+  }
+  filebeat_conf = {
+    "setup.kibana" = {
+      "host" = "https://kibana.example.com:443"
+    },
+    "setup.dashboards.enabled" = true,
+    "setup.template.enabled"   = true,
+    "setup.template.name"      = "filebeat",
+    "setup.template.pattern"   = "filebeat-*",
+    "setup.template.settings" = {
+      "index.number_of_shards" = 1
+    },
+    "setup.ilm.enabled"      = "auto",
+    "setup.ilm.check_exists" = false,
+    "setup.ilm.overwrite"    = true,
+    "filebeat.modules" = [
+      {
+        "module" = "system",
+        "syslog" = {
+          "enabled" = true
+        },
+        "auth" = {
+          "enabled" = true
+        }
+      }
+    ],
+    "filebeat.inputs" = [
+      {
+        "type" = "container",
+        "paths" = [
+          "/var/log/containers/*.log"
+        ],
+        "stream" = "all",
+        "processors" = [
+          {
+            "add_kubernetes_metadata" = {
+              "host" = "$${NODE_NAME}",
+              "matchers" = [
+                {
+                  "logs_path" = {
+                    "logs_path" = "/var/log/containers/"
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
+    ],
+    "filebeat.autodiscover" = {
+      "providers" = [
+        {
+          "type"          = "kubernetes",
+          "hints.enabled" = true,
+          "templates" = [
+            {
+              "condition.equals" = {
+                "kubernetes.labels.logging" = true
+              },
+              "config" = [
+                {
+                  "type" = "container",
+                  "paths" = [
+                    "/var/log/containers/*-$${data.kubernetes.container.id}.log"
+                  ],
+                  "exclude_lines" = [
+                    "^\\s+[\\-`('.|_]"
+                  ]
+                }
+              ]
+            },
+            {
+              "condition.equals" = {
+                "kubernetes.labels.logtype" = "nginx"
+              },
+              "config" = [
+                {
+                  "module" = "nginx",
+                  "access" = {
+                    "enabled" = true,
+                    "var.paths" = [
+                      "/var/log/nginx/access.log*"
+                    ]
+                  },
+                  "error" = {
+                    "enabled" = true,
+                    "var.paths" = [
+                      "/var/log/nginx/error.log*"
+                    ]
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    "processors" = [
+      {
+        "add_cloud_metadata" = null
+      },
+      {
+        "decode_json_fields" = {
+          "fields" = [
+            "message"
+          ],
+          "process_array"  = false,
+          "max_depth"      = 1,
+          "target"         = "",
+          "overwrite_keys" = true,
+          "add_error_key"  = true
+        }
+      },
+      {
+        "drop_event" = {
+          "when" = {
+            "not" = {
+              "contains" = {
+                "kubernetes.pod.labels.logging" = "true"
+              }
+            }
+          }
+        }
+      },
+      {
+        "drop_event" = {
+          "when" = {
+            "regexp" = {
+              "message" = "(?i)kube-probe/1.18+"
+            }
+          }
+        }
+      },
+      {
+        "drop_event" = {
+          "when" = {
+            "regexp" = {
+              "message" = "(?i)ELB-HealthChecker/2.0"
+            }
+          }
+        }
+      }
+    ]
+  }
+
+}
